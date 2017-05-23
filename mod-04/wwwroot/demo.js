93090c5b3dcb965846ba43f0723998703e5197b3
Maurice de Beijer <maurice.de.beijer@gmail.com>
2017-05-23 11:35:35 +0200

    Ajax requests

--- mod-04/wwwroot/demo.js
+++ mod-04/wwwroot/demo.js
@@ -1,5 +1,35 @@
-(function () {
 
-  angular.module("myApp", [])
+(function() {
+  angular.module("myApp", ['ngResource'])
+    .controller("MoviesController", function($scope, Movies) {
+    // $scope.movies = [
+    //   {
+    //     title: "2001: A Space Odyssey"
+    //   }, {
+    //     title: "Star wars"
+    //   }
+    // ];
 
+    //  Movies.getAllMovies()
+    //   .then(movies => $scope.movies = movies)
+      
+      // $scope.movies = Movies.query()
+$scope.movies = [
+  Movies.get({movieId: 369557})
+]
+
+      // console.log( $scope.movies.length)
+
+      // setTimeout(() => console.log( $scope.movies.length), 1000);
+      //  $scope.movies.$promise.then(() =>  console.log( $scope.movies.length));
+  })
+  // .service('Movies', function($http){
+  //   this.getAllMovies = function(){
+  //   return $http.get('/api/movies')
+  //       .then(rsp => rsp.data)     
+  //   }
+  // });
+  .factory('Movies', function($resource){
+    return $resource('/api/movies/:movieId');
+  })
 })();
